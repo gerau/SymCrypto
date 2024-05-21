f938a63515c0a7bcb54b4622274aca187ccd4e5e
Volodymyr Herashchenko <cholkegn@gmail.com>
2024-05-21 11:45:56 +0300

    lab4 initial commit

---
+++ lab4/GeffeGenerator/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GeffeGenerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
